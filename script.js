e94e9296f14a30c6956ae67b1e4020ac7f4eedd9
Hady Mohammed <hady.abdelfattah@icloud.com>
2023-06-11 09:57:06 +0200

    finish

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+var i;
 var images = [];
 
-images = [" image1"," image2"," image5",];
-
-console.log(images.length);
+images = [" image1"," image2"," image3","image4","image5"];
+images[4] = "image222";
+images.push("image6");
+console.log(images);
