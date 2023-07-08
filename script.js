d424d5d8deb145d0a66c6adf1babb318d2b7095f
Hady Mohammed <hady.abdelfattah@icloud.com>
2023-07-08 18:27:42 +0200

    latest

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var menuicon = document.getElementById("menu470");
 var list = document.getElementById("list");
+var menustatus;
+menustatus = "closed";
+
 menuicon.onclick = function(){
-   menuicon.src = "cross.png";
-   list.style.display = "block";
+   if(menustatus == "closed"){
+      menuicon.src = "cross.png";
+      list.style.display = "block";
+      menustatus = "open";
+   } else{
+      menuicon.src = "list.png";
+   list.style.display = "none";
+   menustatus = "closed";
+   }
 }
 
